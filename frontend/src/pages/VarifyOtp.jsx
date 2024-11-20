48a77f0d38cf2eea207b0d49178a7f68911d85c6
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2024-11-20 13:20:03 +0530

    Update VarifyOtp.jsx

--- frontend/src/pages/VarifyOtp.jsx
+++ frontend/src/pages/VarifyOtp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-const API_BASE_URL="http://localhost:4000"
+const API_BASE_URL="https://mern-booking-app-2ng3.onrender.com"
 
 const OTPVerification = () => {
   const [email, setEmail] = useState('');
