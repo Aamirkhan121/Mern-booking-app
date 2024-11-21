a6ba38c16739aabdcb45968f6c26e9abbaa73bbb
Aamirkhan121 <aamir73690@gmail.com>
2024-11-21 12:26:50 +0530

    commit header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,13 +22,8 @@ const Header = ({ isAuthenticated, handleLogout }) => {
   }, [localStorage.getItem('firstName')]); // Dependency array makes sure it reruns when the firstName in localStorage changes
 
   return (
-<<<<<<< HEAD
      <header className=" z-99 sticky top-0 bg-blue-800 py-3 z-50 w-full shadow-lg">
       <div className="container mx-auto flex flex-wrap justify-between items-center px-4 md:px-8">
-=======
-     <header className="sticky top-0 bg-blue-800 py-3 z-50 w-full shadow-lg">
-      <div className="container mx-auto flex justify-between items-center px-4 md:px-8">
->>>>>>> d9fa73de00d1a5d511431826dc15cd31cd89367d
         {/* Website Logo or Name */}
         <span className="text-white text-xl font-bold">
           <Link to="/" className="hover:text-gray-300 text-[15px] ml-[-12px]">
@@ -57,15 +52,6 @@ const Header = ({ isAuthenticated, handleLogout }) => {
               >
                 My Booking
               </Link>
-<<<<<<< HEAD
-=======
-              <Link
-                to="/my-hotels"
-                className="text-white hover:text-gray-300 text-sm md:text-base"
-              >
-                My Hotels
-              </Link>
->>>>>>> d9fa73de00d1a5d511431826dc15cd31cd89367d
               <button
                 onClick={handleLogout}
                 className="bg-red-600 text-white px-4 py-2 text-sm md:text-base rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 transition duration-150"
@@ -75,15 +61,6 @@ const Header = ({ isAuthenticated, handleLogout }) => {
             </div>
           ) : (
             <div className="flex space-x-4">
-<<<<<<< HEAD
-=======
-              {/* Sign In and Sign Up Buttons */}
-              <Link to="/sign-in">
-                <button className="bg-blue-600 text-white font-bold py-2 px-6 text-sm md:text-base rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 transition duration-150">
-                  Sign In
-                </button>
-              </Link>
->>>>>>> d9fa73de00d1a5d511431826dc15cd31cd89367d
               <Link to="/sign-up">
                 <button className="bg-green-600 text-white font-bold py-2 px-6 text-sm md:text-base rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 transition duration-150">
                   Sign Up
